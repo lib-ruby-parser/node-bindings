ed87448b09296ceedd9f598f00bb90ebf2adb5a3
Ilya Bylich <ibylich@gmail.com>
2021-01-30 01:21:13 +0300

    updated release workflow

--- pkg/index.js
+++ pkg/index.js
@@ -1,10 +1,13 @@
 switch (process.platform) {
     case 'linux':
-        module.exports = require('./linux-x86_64.node');
+        module.exports = require('./linux.node');
         break;
     case 'darwin':
-        module.exports = require('./darwin-x86_64.node')
+        module.exports = require('./darwin.node')
+        break;
+    case 'win32':
+        module.exports = require('./win32.node');
         break;
     default:
-        throw new Error(`unsupported process.platform '${process.platform}' (only 'linux' and 'darwin' are supported)`);
+        throw new Error(`unsupported process.platform '${process.platform}' (only 'linux', 'darwin' and 'win32' are supported)`);
 }
